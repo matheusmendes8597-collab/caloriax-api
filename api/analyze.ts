a3bdc13abdc1beb8f13ab3789389745718754d1b
Matheus Henrique <matheusmendes8597@gmail.com>
2026-05-10 03:15:37 -0300

    Add x-api-key to CORS headers

--- api/analyze.ts
+++ api/analyze.ts
@@ -19,7 +19,10 @@ export default async function handler(req: any, res: any) {
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, x-api-key"
+  );
 
   if (req.method === "OPTIONS") return res.status(200).end();
 
